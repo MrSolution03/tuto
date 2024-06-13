bfe75183c7bd7ed87c352458f93dd21eceaf51fb
Tech_Solution <163984462+MrSolution03@users.noreply.github.com>
2024-06-13 20:50:18 +0300

    counter app(use of useState)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function MyApp(){
+
+  const [count, setCount]= useState(0);
+
+  function handleClick(){
+    setCount(count+1);
+  }
+  return(
+    <>
+    <h1>Counters that update separately</h1>
+    <MyButton count={count} onClick={handleClick}/>
+    <MyButton count={count} onClick={handleClick}/>
+    </>
+  )
+
 }
 
-export default App;
+function MyButton({count, onClick}){
+  return(
+    <button onClick={onClick}>Click {count} times</button>
+  )
+}
